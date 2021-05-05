ff384cd5180eb02e36487f362755f37d69059f7a
eloeloelo320 <tomekwolos@gmail.com>
2021-05-05 13:33:34 +0200

    level1v2

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform player;
+    void FixedUpdate ()
+    {
+        transform.position = new Vector3(player.position.x,player.position.y,transform.position.z);
+    }
+}
