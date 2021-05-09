acc7fb9752cc903141e6b747b93fee6b79a39c5b
caraczan <przemek.banaszek@protonmail.com>
2021-05-09 23:36:04 +0200

    script death preparation for future encounters, moved player movement scripts to scripts/playerscripts

--- Assets/Scripts/PlayerScripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerScripts/PlayerDeath.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D col) {
-		if (col.gameObject.CompareTag("Enemy")) {
+		if(col.gameObject.CompareTag("Abyss")) {
+			Destroy(gameObject);
+			LevelManager.instance.Respawn();
+		}
+
+		if(col.gameObject.CompareTag("Enemy")) {
+			// TODO: On enemy hit player should get hit and receive it,
 			Destroy(gameObject);
 			LevelManager.instance.Respawn();
 		}
