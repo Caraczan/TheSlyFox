6e1d6746a79b03d2776201ff5ecdb24d3a2406d9
Caraczan <przemek.banaszek@protonmail.com>
2021-05-09 15:02:51 +0200

    rolled back previous commit, player respawns upn touching object with tag Enemy

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+    
+
+    private void Awake(){
+    	instance = this;
+    }
+    public void Respawn() {
+    	GameObject player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+    } 
+}
