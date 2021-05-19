f2b9a98a3349018ada5ad9dc9baf49e154810e96
eloeloelo320 <tomekwolos@gmail.com>
2021-05-19 12:33:36 +0200

    lev 1-3

---
+++ Assets/InfiniteMap2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteMap2 : MonoBehaviour
+{
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+    }
+}
