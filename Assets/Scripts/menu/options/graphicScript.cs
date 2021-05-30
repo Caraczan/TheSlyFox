523422f941a76cc0fd5a251fd3e8c14dc43d002a
291578umk <39299737+majumaster@users.noreply.github.com>
2021-05-30 15:15:21 +0200

    opcje rozbudowane
    dodane rozdzialki i opcja fullSC i windowed

---
+++ Assets/Scripts/menu/options/graphicScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphicScript : MonoBehaviour
+{
+    List<int> widths = new List<int>() { 1920, 1280, 960, 800 };
+    List<int> heights = new List<int>() { 1080,800,540,600 };
+    List<bool> screenSet = new List<bool>() {true,false };
+    public void SetScreenSize(int index)
+    {
+        Screen.SetResolution(widths[index], heights[index], Screen.fullScreen);
+    }
+    public void SetFullScreen(int index)
+    {
+        Screen.fullScreen = screenSet[index];
+    }
+}
