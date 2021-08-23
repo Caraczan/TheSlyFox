b472954aaacdf09a391b0a27a0896118ae1d7335
madzastik <madzastik@gmail.com>
2021-08-23 11:59:53 +0200

    'timer'

---
+++ Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.text = startTime.ToString("f2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTime += Time.deltaTime;
+        timerText.text = startTime.ToString("f2");
+    }
+}
