6b7485a9ea95df93e41bb21a1f5e1b3a79a965bb
madzastik <madzastik@gmail.com>
2021-08-25 10:16:01 +0200

    tracenie życia po upadku2

--- Assets/Scripts/PlayerScripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerScripts/PlayerDeath.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerDeath : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D col) {
 		if(col.gameObject.CompareTag("Abyss")) {
