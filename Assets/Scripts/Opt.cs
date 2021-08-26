eccc9020792b6d413f8c1f3bf3fa8cefced3cbf0
madzastik <madzastik@gmail.com>
2021-08-26 11:51:02 +0200

    dodanie sceny smierci(brak implementacji przycisków)

---
+++ Assets/Scripts/Opt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Opt : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+   
+
+
+}
