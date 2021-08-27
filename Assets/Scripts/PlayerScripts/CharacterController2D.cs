d3ec235371ed98c72b1565b2ef0870e5af14465e
eloeloelo320 <tomekwolos@gmail.com>
2021-08-27 22:01:11 +0200

    collectibles

--- Assets/Scripts/PlayerScripts/CharacterController2D.cs
+++ Assets/Scripts/PlayerScripts/CharacterController2D.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CharacterController2D : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+
+	[SerializeField] private int diamonds = 0;
+	[SerializeField] private Text diamondText;	
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
@@ -146,6 +150,16 @@ public class CharacterController2D : MonoBehaviour
 				m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce*doubleJumpForce));
 				canDoubleJump = false;
 			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Collectible")
+		{
+			Destroy(collision.gameObject);
+			diamonds += 1;
+			diamondText.text = diamonds.ToString();
 		}
 	}
 
