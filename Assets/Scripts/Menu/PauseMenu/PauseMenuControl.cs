c60db19865df84ffcbd1e2c4c36d06653198c6a5
291578umk <39299737+majumaster@users.noreply.github.com>
2021-09-15 14:06:18 +0200

    poprawki
    usunalem zbedne klawisze, posegregowałem swoje skrypty itp

--- Assets/Scripts/Menu/PauseMenu/PauseMenuControl.cs
+++ Assets/Scripts/Menu/PauseMenu/PauseMenuControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class pauseMenu : PauseMenuS
+public class PauseMenuControl : PauseMenuS
 {
     
     public void ResumeGame()
